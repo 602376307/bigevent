3c2ba2e2e52971ce8f8788175e2d4dd3c59c9961
xiniuyuanshuai <602376307@qq.com>
2021-12-07 08:40:13 +0800

    完成了首页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,19 @@
 $.ajaxPrefilter(function (options) {
     // options.url为$.ajax中的url，因此进行拼接前缀,进行赋值
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    // 
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 挂载complete函数 无论请求成功与否都会执行该回调
+    options.complete = function (res) {
+        if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+            // 清空token
+            localStorage.removeItem('token')
+            // 跳转回登录页面
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function () {
+    // 请求用户信息
+    getUserInfo()
+    // 退出功能
+    var layer = layui.layer // 导入layer
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确认退出登录吗？', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            // 清空token
+            localStorage.removeItem('token')
+            // 跳转回登录页
+            location.href = '/login.html'
+            layer.close(index); // 关闭询问框
+        });
+    })
+
+})
+// 请求用户信息函数
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // 请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('请求用户信息失败')
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+// 渲染用户头像函数
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic) {
+        $('.avatar').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.avatar').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
